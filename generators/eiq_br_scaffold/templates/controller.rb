b0b406f574ac76e04ddd2285241b81a8df64333f
Kivanio Barbosa <kivanio@gmail.com>
2008-08-06 09:41:05 -0400

    1 - alterndo metodo index para trazer somente os nao deletados

--- generators/eiq_br_scaffold/templates/controller.rb
+++ generators/eiq_br_scaffold/templates/controller.rb
@@ -9,7 +9,7 @@ class <%= controller_class_name %>Controller < ApplicationController
     sort_init 'updated_at'
     sort_update
     
-    @<%= table_name %> = <%= class_name %>.paginate :page => params[:page], :per_page => 10, :order => sort_clause
+    @<%= table_name %> = <%= class_name %>.visivel.paginate :page => params[:page], :per_page => 10, :order => sort_clause
   end
 
   # GET /<%= table_name %>/1
